1199bc0ea4753605716e32eb0a67438bf046f59c
gmorae <gmmartins06@gmail.com>
2019-11-13 14:16:55 -0200

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+
+function DataFromatada(props) {
+  return <h2>Horário atual: {props.date.toLocaleTimeString()}</h2>
+}
+
+class Clock extends Component{
+  constructor(props){
+    super(props) //permite que usa o this
+    this.state = {
+      date: new Date()
+    }
+
+  }
+
+  // Ciclo de vida que ocorre quando o componente é inserido o DOM
+  componentDidMount(){
+    this.TimerId = setInterval( () => {
+      this.thick()
+    })
+    console.log("relogio => " + this.TimerId)
+  }
+
+  // Ciclo de vida que ocorre quando o compnente é removido do DOM 
+  componentWillMount(){
+    clearInterval(this.TimerId)
+  }
+
+  thick(){
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  // Renderiza na tela o titulo e o retorno da função DataFormatada
+  // Passando date com o valor atual do state
+  render(){
+    return(
+      <div>
+        <h1>Relógio</h1>
+        <DataFromatada date= {this.state.date} />
+      </div>
+    )
+  }
+}
+
+// Função principal invocada pelo index.js
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Clock />
       </header>
     </div>
   );
